36876fc2605a58783e5e7d1d3f5646fad0a518a0
angelc29 <angelf.2599@gmail.com>
2022-12-01 20:55:18 -0600

    se agregó la libreria import-export

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'tasks',
+    'import_export'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_URL = '/signin'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,21 @@
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from django.contrib import admin
 from .models import Task
 
-class TaskAdmin(admin.ModelAdmin):
+# Se definen los campos que van a ser exportados con la libreria
+# La sintaxis es parecida a los forms de django
+class TaskResources(resources.ModelResource):
+    class Meta:
+        model = Task
+        fields = ('title', 'description', 'important', 'datecompleted', 'id')
+
+# Se remplaza admin.ModelAdmin por ImportExportModelAdmin
+class TaskAdmin(ImportExportModelAdmin):
+    resource_class = TaskResources
     readonly_fields = ("created",)
+
 
 # Register your models here.
 # Registra los modelos para poder ser utilizados en el panel del admin
